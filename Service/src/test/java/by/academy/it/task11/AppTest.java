a6025373c0d7d37ac1c5e48caae6877af5d726a0
extensaman <you@example.comverus.wedding@gmail.com>
2022-04-29 14:01:03 +0300

    Initial commit

---
+++ Service/src/test/java/by/academy/it/task11/AppTest.java
@@ -0,0 +1,20 @@
+package by.academy.it.task11;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends Assertions
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
